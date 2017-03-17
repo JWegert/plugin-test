449eed83464ec21fdfa8e4693d4eaac6c631ccb0
jweg <jweg@informatik.uni-kiel.de>
2017-03-17 11:53:50 +0100

    error .lookupFactory is not a function

--- addon/instance-initializers/addroute.js
+++ addon/instance-initializers/addroute.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-export function initialize() {
-
+export function initialize(applicationInstance) {
+let container = applicationInstance.container;
+let router = container.lookupFactory('router:main');
 	//let router = applicationInstance.lookupFactory('router:main');
-	let router = Ember.getOwner(this).lookup('router:main');
 
   router.map(function(){
 	this.route('gittest');
