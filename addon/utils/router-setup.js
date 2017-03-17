2463b8690a43024dbc1a475c14234fa44115dae2
jweg <jweg@informatik.uni-kiel.de>
2017-03-17 10:28:17 +0100

    register addon route

---
+++ addon/utils/router-setup.js
@@ -0,0 +1,3 @@
+export default function routerSetup(router) {
+  router.route('gittest');
+}
---
+++ tests/unit/utils/router-setup-test.js
@@ -0,0 +1,10 @@
+import routerSetup from 'dummy/utils/router-setup';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | router setup');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = routerSetup();
+  assert.ok(result);
+});
