fb227ce286e266de14c67a4896cf86c23b3310ff
jweg <jweg@informatik.uni-kiel.de>
2017-03-17 14:54:19 +0100

    Service testing

--- addon/routes/gittest.js
+++ addon/routes/gittest.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	testService: Ember.inject.service("landscape-reload"),
-
-model: function() {
-	return this.testService.updateObject();
-}
+	
 });
 
