90e4d1c54679879c008e9fb1b3ba899e8718d639
jweg <jweg@informatik.uni-kiel.de>
2017-03-21 11:52:30 +0100

    inject service

--- addon/components/comp-test.js
+++ addon/components/comp-test.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import layout from '../templates/components/comp-test';
 
 export default Ember.Component.extend({
-  hostService: Ember.inject.service('plugintest'),
+  pluginTest: Ember.inject.service('plugintest'),
   message: 'test',
   actions: {
   	pressMe: function () {
-  		var testText = this.get('plugintest').thisistest();
+  		var testText = this.get('pluginTest').thisistest();
   		this.set('message', testText);
-  		console.log(this.get('plugintest').isAuthenticated);
+  		console.log(this.get('pluginTest').isAuthenticated);
   	}
   }
 });
