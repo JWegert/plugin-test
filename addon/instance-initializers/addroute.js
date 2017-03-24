989095479ecfe419a295dc7391a0182731fed8db
jweg <jweg@informatik.uni-kiel.de>
2017-03-24 16:30:15 +0100

    add button for addon

--- addon/instance-initializers/addroute.js
+++ addon/instance-initializers/addroute.js
@@ -6,6 +6,12 @@ let router = applicationInstance.__container__.lookupFactory('router:main');
   router.map(function(){
 	this.route('gittest');
 });
+
+  navbarService: Ember.inject.service('navbar'),
+activate(){
+	this.get('navbarService').navbarLabels.addObject('another');
+}
+
 }
 
 export default {
