fe372ddceff8be50e9def232f90b15996153d2df
jweg <jweg@informatik.uni-kiel.de>
2017-03-28 09:55:22 +0200

    add addon menu item

--- addon/routes/gittest.js
+++ addon/routes/gittest.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+navbarService: Ember.inject.service('navbar'),
+
+activate(){
+	this.get('navbarService').navbarLabels.addObject('gittest');
+},
 	
 });
-
